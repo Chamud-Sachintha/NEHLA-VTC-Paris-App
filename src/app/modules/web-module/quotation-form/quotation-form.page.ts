a4002cc346510d611e49fe675d376b5f862e37e0
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-05-03 19:24:24 +0530

    select vehicle logic create

--- src/app/modules/web-module/quotation-form/quotation-form.page.ts
+++ src/app/modules/web-module/quotation-form/quotation-form.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule } from '@ionic/angular';
 import { DataShareService } from 'src/app/services/data-share.service';
 import { Router } from '@angular/router';
 import { JouneyForm } from 'src/app/models/jouney-form';
@@ -24,8 +24,12 @@ export class QuotationFormPage implements OnInit {
   };
   apiLoaded!: Observable<boolean>;
   journeyForm = new JouneyForm();
+  selectedVehicle!: string;
+  firstVehicle: boolean = false;
+  secondVehicle: boolean = false;
 
-  constructor(public dataShareService: DataShareService, public router: Router, httpClient: HttpClient) {
+  constructor(public dataShareService: DataShareService, public router: Router, httpClient: HttpClient
+            , public alertController: AlertController) {
     this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyB1ZVPSd5Nux1uSrVM59Srk7G9-uXxSfuM&amp', 'callback')
       .pipe(
         map(() => true),
@@ -51,6 +55,36 @@ export class QuotationFormPage implements OnInit {
   }
 
   goToPersonalFormPage() {
-    this.router.navigate(['/booking/personal-form']);
+
+    if (this.selectedVehicle == null) {
+      this.openThePopupModelForValidation('Vehicle is Required', null, 'Please Select a Vehicle.');
+    } else {
+      this.router.navigate(['/booking/personal-form']);
+    }
+  }
+
+  vehicleSelectAction(vehicleName: string, vehicleNumber: number) {
+
+    if (vehicleNumber == 1) {
+      this.firstVehicle = true;
+      this.secondVehicle = false;
+    }
+
+    if (vehicleNumber == 2) {
+      this.firstVehicle = false;
+      this.secondVehicle = true;
+    }
+    this.selectedVehicle = vehicleName;
+  }
+
+  async openThePopupModelForValidation(header: string, subHeader: any = null, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      subHeader: subHeader,
+      message: message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 }
